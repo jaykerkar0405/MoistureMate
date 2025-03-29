32f7f88ea59c671ab9d99a05a208600b9b3676fb
jaykerkar0405 <bytewise0405@gmail.com>
2025-03-29 21:22:38 +0530

    style: update app styling and layout for improved user experience; integrate authentication components

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,8 @@
+import { redirect } from '@sveltejs/kit';
+
 interface Event {
 	locals: Locals;
+	url: URL;
 }
 
 interface Session {
@@ -7,13 +10,21 @@ interface Session {
 }
 
 interface Locals {
-	auth: () => Promise<Session>;
+	auth: () => Promise<Session | null>;
 }
 
 export const load = async (event: Event) => {
-	const session: Session = await event.locals.auth();
+	const protectedRoutes = ['/'];
+	const pathname = event.url.pathname;
+	const session = await event.locals.auth();
 
-	return {
-		session
-	};
+	if (!session && protectedRoutes.includes(pathname)) {
+		throw redirect(302, '/auth');
+	}
+
+	if (session && pathname === '/auth') {
+		throw redirect(302, '/');
+	}
+
+	return { session };
 };
